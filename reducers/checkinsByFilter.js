fa83f5a245827178aec6fa365ce7eadf7c44d97d
Otis <altispec@gmail.com>
2015-10-25 13:34:51 +0800

    Add reducers and actions of fetching checkins by filter

---
+++ reducers/checkinsByFilter.js
@@ -0,0 +1,44 @@
+import merge from 'lodash/object/merge';
+import {
+  FETCH_CHECKINS_START,
+  FETCH_CHECKINS_SUCCESS,
+  FETCH_CHECKINS_FAILED,
+  LOAD_CHECKINS,
+  SET_DISTANCE_FILTER
+} from '../actions';
+
+
+const initialState = {
+  isFetching: false,
+  distanceFilter: 50,
+  checkins: []
+};
+
+export default function checkinsByFilter(state = initialState, action) {
+  switch (action.type) {
+  case FETCH_CHECKINS_START:
+    return merge({}, state, {
+      isFetching: true,
+    });
+  case FETCH_CHECKINS_SUCCESS:
+    return merge({}, state, {
+      isFetching: false,
+      response: action.response
+    });
+  case FETCH_CHECKINS_FAILED:
+    return merge({}, state, {
+      isFetching: false,
+      error: action.error
+    });
+  case SET_DISTANCE_FILTER:
+    return merge({}, state, {
+      distanceFilter: action.filter
+    });
+  case LOAD_CHECKINS:
+    return merge({}, state, {
+      checkins: action.checkins
+    });
+  default:
+    return state;
+  }
+}
--- reducers/index.js
+++ reducers/index.js
@@ -1,34 +1,12 @@
 import merge from 'lodash/object/merge';
 import { routerStateReducer as router } from 'redux-router';
 import { combineReducers } from 'redux';
-// import {
-//   SET_DISTANCE_FILTER,
-//   LOAD_CHECKINS
-// } from '../actions';
 
 import geolocation from './geolocation';
-
-// const initialState = {
-//   distanceFilter: 50,
-//   checkins: []
-// };
-//
-// function checkinApp(state = initialState, action) {
-//   switch (action.type) {
-//   case SET_DISTANCE_FILTER:
-//     return merge({}, state, {
-//       distanceFilter: action.filter
-//     });
-//   case LOAD_CHECKINS:
-//     return merge({}, state, {
-//       checkins: action.checkins
-//     });
-//   default:
-//     return state;
-//   }
-// }
+import checkinsByFilter from './checkinsByFilter';
 
 const rootReducer = combineReducers({
+  checkinsByFilter,
   geolocation,
   router
 });
