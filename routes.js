5deb383eb2229adbf2630dc107d5818390779579
Otis <altispec@gmail.com>
2015-10-26 11:51:08 +0800

    Add route plan

--- routes.js
+++ routes.js
@@ -4,11 +4,22 @@ import App from './containers/App';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
 
-export default (
-  <Route path='/' component={App}>
-    <IndexRoute component={Home}/>
+export default (store) => {
+  return (
+    <Route path='/' component={App}>
+      <IndexRoute component={Home}/>
+      { /* Routes
+      <Route onEnter={requireLogin}>
+        <Route path='checkin' component={Checkin}/>
+      </Route>
+      */ }
+      { /* Routes
+      <Route path='login' component={Login}/>
+      <Route path='signup' component={Signup}/>
+      */ }
 
-    { /* Catch all route */ }
-    <Route path='*' component={NotFound} status={404} />
-</Route>
-);
+      { /* Catch all route */ }
+      <Route path='*' component={NotFound} status={404} />
+    </Route>
+  );
+};
